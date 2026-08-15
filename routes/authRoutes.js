52116b85db3ddc50bba969edf046c842571e96ed
Ilo Tech <ilo.nikatsadze@gmail.com>
2026-08-15 21:43:16 +0400

    add refresh route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,10 +3,12 @@ import login from "../controllers/authControllers/login.js";
 import signup from "../controllers/authControllers/signup.js";
 import logout from "../controllers/authControllers/logout.js";
 import validateSignUpInput from "../middlewares/validateSignUpInput.js";
+import refreshToken from "../controllers/authControllers/refresh.js";
 const authRouter = Router();
 
 authRouter.post("/login", login);
 authRouter.post("/signup", validateSignUpInput, signup);
 authRouter.post("/logout", logout);
+authRouter.post("/refresh", refreshToken);
 
 export default authRouter;
